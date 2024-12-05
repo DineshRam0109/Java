677afae8bf43025f48a0441d1359e0adea8651ad
DineshRam0109 <141487388+DineshRam0109@users.noreply.github.com>
2024-12-06 01:26:49 +0530

    d_inch.java

---
+++ inch.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class d_inch {
+public static void main(String[] args) {
+Scanner s = new Scanner(System.in);
+System.out.print("Input a value for inch: ");
+double inches = sc.nextDouble();
+System.out.println(inches + " inch is " + inches*0.0254 + " meters");
+}
+}
